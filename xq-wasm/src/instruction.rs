44507a07a43932c46d6b1beb9c14e59d34213252
许强 <Qiang.Xu9@geely.com>
2022-07-28 18:35:12 +0800

    add wasm instruction 1

--- xq-wasm/src/instruction.rs
+++ xq-wasm/src/instruction.rs
@@ -1,6 +1,50 @@
 
+use crate::module::LabelIndex;
+
 pub trait Expr {
     fn new(&self){
         todo!()
     }
+}
+
+pub struct Instruction{
+    opcode: u8,
+    args: Args,
+}
+
+pub enum Args{
+    None,
+    MemArg,
+    BlockArgs,
+    IfArgs,
+    BrTableArgs,
+    u32,
+    u64,
+    i32,
+    i64,
+    f32,
+    f64,
+}
+
+pub struct MemArg {
+    pub offset: u32,
+    pub align:  u32,
+}
+
+type BlockType = i32;
+
+pub struct BlockArgs {
+    bt: BlockType,
+    instrs: Vec<Instruction>
+}
+
+pub struct IfArgs {
+    bt: BlockType,
+    instr1: Vec<Instruction>,
+    instr2: Vec<Instruction>,
+}
+
+pub struct BrTableArgs {
+    labels: Vec<LabelIndex>,
+    default: LabelIndex,
 }
